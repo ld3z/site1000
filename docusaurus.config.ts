2c801b85031e048fa3bfe35a0c6fe4a765f94249
ld3zian <42660576+ld3z@users.noreply.github.com>
2025-12-11 14:27:22 -0700

    fix img zoom

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -143,6 +143,16 @@ const config: Config = {
       theme: prismThemes.github,
       darkTheme: prismThemes.dracula,
     },
+    zoom: {
+      selector: ".markdown :not(em) > img",
+      background: {
+        light: "rgb(255, 255, 255)",
+        dark: "rgb(50, 50, 50)",
+      },
+      config: {
+        // options you can specify via https://github.com/francoischalifour/medium-zoom#usage
+      },
+    },
   } satisfies Preset.ThemeConfig,
 };
 
